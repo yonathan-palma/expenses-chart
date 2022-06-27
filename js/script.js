fd1b1b702dbaf062f5a560bc952e529e542e6cd6
yonathan <yonathanpalmarey@gmail.com>
2022-06-26 21:10:07 -0400

    finalizando

--- js/script.js
+++ js/script.js
@@ -39,39 +39,39 @@ const upload_bar = ()=>{
         .then(data => { 
             const mon =document.getElementById("mon");
             mon.setAttribute("data-amount", `$${data[0].amount}`);
-            // mon.style.height = `${data[0].amount}%`;
-            updateBAr(mon, data[0].amount);
+            mon.style.height = `${data[0].amount}%`;
+            // updateBAr(mon, data[0].amount);
             // move(mon, data[0].amount);
             
             const tue =document.getElementById("tue");
             tue.setAttribute("data-amount", `$${data[1].amount}`);
-            // tue.style.height = `${data[1].amount}%`;
+            tue.style.height = `${data[1].amount}%`;
             // move(tue, data[1].amount);
 
             const wed =document.getElementById("wed");
             wed.setAttribute("data-amount", `$${data[2].amount}`);
-            updateBAr(wed, data[2].amount);
-            // wed.style.height = `${data[2].amount}%`;
+            // updateBAr(wed, data[2].amount);
+            wed.style.height = `${data[2].amount}%`;
             // move(wed, data[2].amount);
 
             const thu =document.getElementById("thu");
             thu.setAttribute("data-amount", `$${data[3].amount}`);
-            // thu.style.height = `${data[3].amount}%`;
+            thu.style.height = `${data[3].amount}%`;
             // move(thu, data[3].amount);
 
             const fri =document.getElementById("fri");
             fri.setAttribute("data-amount", `$${data[4].amount}`);
-            // fri.style.height = `${data[4].amount}%`;
+            fri.style.height = `${data[4].amount}%`;
             // move(fri, data[4].amount);
 
             const sat =document.getElementById("sat");
             sat.setAttribute("data-amount", `$${data[5].amount}`);
-            // sat.style.height = `${data[5].amount}%`
+            sat.style.height = `${data[5].amount}%`
             // move(sat, data[5].amount);
 
             const sun =document.getElementById("sun");
             sun.setAttribute("data-amount", `$${data[6].amount}`);
-            // sun.style.height = `${data[6].amount}%`;
+            sun.style.height = `${data[6].amount}%`;
             // move(sun, data[6].amount);
         })
         .catch(() => {
